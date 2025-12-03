69171f52cc8a4d9e979e2e83be9f002714c531c3
Suraj Subedi <surajsubedi@Surajs-MacBook-Pro.local>
2025-12-02 19:30:32 -0500

    Added more email validation test cases

--- BasicSample/app/src/test/java/com/example/android/testing/espresso/BasicSample/EmailValidator.kt
+++ BasicSample/app/src/test/java/com/example/android/testing/espresso/BasicSample/EmailValidator.kt
@@ -9,4 +9,19 @@ class EmailValidatorTest {
     fun testCorrectEmailFormat() {
         assertTrue(EmailValidator.isValidEmail("123@abc.com"))
     }
+
+    @Test
+    fun testCorrectSubdomainEmail() {
+        assertTrue(EmailValidator.isValidEmail("123@abc.co.ca"))
+    }
+
+    @Test
+    fun testIncorrectDomain() {
+        assertFalse(EmailValidator.isValidEmail("123@abc"))
+    }
+
+    @Test
+    fun testDoubleDots() {
+        assertFalse(EmailValidator.isValidEmail("123@abc..com"))
+    }
 }
